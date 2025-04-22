8430bf8a350ebc42d390aebd1160bb332a1371c4
NazmulHasnat24 <nhasnat24@gmail.com>
2025-04-22 23:35:05 +0600

    Initial commit

--- String.dart
+++ String.dart
@@ -2,5 +2,8 @@ main(){
   String name = "Esti";
   String name2 = "Hasnat";
   // string print type1
-  print("My name is ${name + name2}");
+  print("My name is ${name +" "+ name2}");
+  // string print type2
+  String input = "$name $name2";
+  print("My name is $input");
 }
