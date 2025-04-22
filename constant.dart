d550215746da3e32aab614702927efac639d6dbd
NazmulHasnat24 <nhasnat24@gmail.com>
2025-04-23 00:49:02 +0600

    Initial commit

---
+++ constant.dart
@@ -0,0 +1,12 @@
+main(){
+  const List <int> number = [1,2,3];
+  print(number);
+
+
+  // const keyword cannot add,remove variables or attributes it is fully set
+  number.add(4);
+  print(number);
+
+ // can access number
+  print(number[1]);
+}
