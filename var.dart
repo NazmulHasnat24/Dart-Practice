7f5eb69c8eeee3e6f9c883269b6b665226486545
NazmulHasnat24 <nhasnat24@gmail.com>
2025-04-22 23:14:45 +0600

    Initial commit

---
+++ var.dart
@@ -0,0 +1,9 @@
+main(){
+  int num1 = 10;
+  var num2 = 20.22;
+  var sum = num1+num2;
+  var name =  "Esti";
+  print("Sub is ${num2-num1}");
+  print("Sum is $sum");
+  print("The name is $name");
+}
