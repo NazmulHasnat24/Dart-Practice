8aa29bd9e9b95d425b2cb2f8e9bffcad78ad41d0
NazmulHasnat24 <nhasnat24@gmail.com>
2025-04-24 00:54:36 +0600

    For_in loop practice

---
+++ for_in.dart
@@ -0,0 +1,14 @@
+main(){
+  List<String> fruits = ['Apple','Mango', 'Guava'];
+
+
+//syntax for for in loop   
+// for (var element in collection) {
+//   // use element
+// }
+print("The list are :");
+for(String fruit in fruits){
+  print(fruit);
+}
+
+}
