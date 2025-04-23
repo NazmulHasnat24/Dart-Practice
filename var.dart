7e55339bcd64fb01d1831e7c8d2d74bf5a7a9d9c
NazmulHasnat24 <nhasnat24@gmail.com>
2025-04-23 21:59:19 +0600

    DataType

--- var.dart
+++ var.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 main(){
   int num1 = 10;
   var num2 = 20.22;
@@ -6,4 +8,9 @@ main(){
   print("Sub is ${num2-num1}");
   print("Sum is $sum");
   print("The name is $name");
+  print("The Mul is ${num1*num2}");
+  print("Enter a number:");
+  String ? input = stdin.readLineSync();
+  int num3 = int.parse(input!);
+  print("The number is $num3");
 }
