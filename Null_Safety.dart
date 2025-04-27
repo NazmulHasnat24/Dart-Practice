05b5b8f44b70301ee0112dcc31267306e4f47dc1
Nazmul Hasnat <nhasnat24@gmail.com>
2025-04-27 20:17:22 +0600

    Null Safety Practice

---
+++ Null_Safety.dart
@@ -0,0 +1,11 @@
+main(){
+
+  // int number1; //but here the value is not assigned so it will give error   
+  // print(number1);
+
+  int ? number1;//here value is not assigned so it will take a null value in dafault
+  print(number1);
+
+  number1 =  50;//this is nonnullable variable cause it is assigned
+  print(number1); 
+}
