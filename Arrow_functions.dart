4f43d04d2c88e593576a8576f2ad1178a163202d
Nazmul Hasnat <nhasnat24@gmail.com>
2025-04-27 19:38:33 +0600

    Arrow function practice

--- Arrow_functions.dart
+++ Arrow_functions.dart
@@ -2,6 +2,13 @@ main(){
 
 print(Greet());
 
+int num1 = 10;
+int num2 = 20;
+
+print("The Addition is : ${add(10,20)}");
+
 }
 
 Greet() => ("Hello");
+
+int add(int a, int b) => a + b;// Syntax : datatype variable name (parameters if needed) => expression
